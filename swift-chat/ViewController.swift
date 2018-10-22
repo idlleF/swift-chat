a222e7e475a59c3669f02badf84667e9a2cee9f2
Macbook Pro <macbookpro@macbook-pro-2.local>
2018-10-22 23:57:27 +0300

    Initial Commit

---
+++ swift-chat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  swift-chat
+//
+//  Created by Macbook Pro on 22/10/2018.
+//  Copyright © 2018 Macbook Pro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
