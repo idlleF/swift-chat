2f67138d492a499e45f61f0844e8fcb5177ed67e
Macbook Pro <macbookpro@MacBook-Pro-2.local>
2018-10-23 01:16:54 +0300

    Add side menu

---
+++ swift-chat/Controller/ChannelVC.swift
@@ -0,0 +1,20 @@
+//
+//  ChannelVC.swift
+//  swift-chat
+//
+//  Created by Macbook Pro on 23/10/2018.
+//  Copyright © 2018 Macbook Pro. All rights reserved.
+//
+
+import UIKit
+
+class ChannelVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
+       
+    }
+    
+
+}
