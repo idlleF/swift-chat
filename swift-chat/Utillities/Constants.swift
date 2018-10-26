7c94dd5513f739453114efb8e829c3c5ebf23054
Macbook Pro <macbookpro@MacBook-Pro-2.local>
2018-10-26 19:55:18 +0300

    API tests

--- swift-chat/Utillities/Constants.swift
+++ swift-chat/Utillities/Constants.swift
@@ -8,6 +8,21 @@
 
 import Foundation
 
+typealias  CompletionHandler = (_ Success: Bool) -> ()
+
+let BASE_URL = "https://chatappexms.herokuapp.com/v1/"
+let URL_REGISTER = "\(BASE_URL)account/register"
+let URL_LOGIN = "\(BASE_URL)account/login"
+let URL_USER_ADD = "\(BASE_URL)user/add"
+
+let HEADER = [
+    "Content-Type": "application/json; charset=utf-8"
+]
+
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let UNWIND = "unwindToChannel"
+
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
---
+++ swift-chat/View/RoundedButton.swift
@@ -0,0 +1,32 @@
+//
+//  RoundedButton.swift
+//  swift-chat
+//
+//  Created by Macbook Pro on 26/10/2018.
+//  Copyright © 2018 Macbook Pro. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundedButton: UIButton {
+
+    @IBInspectable var cornerRadius: CGFloat = 3.0 {
+        didSet {
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
+    
+    override func awakeFromNib() {
+        self.setupView()
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        self.setupView()
+    }
+    
+    func setupView() {
+        
+    }
+}
